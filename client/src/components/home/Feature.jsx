90467c585e9ae6535cd37e64e5dadf2994b5a148
Angana Boruah <anganaboruah35@gmail.com>
2025-10-26 11:58:17 +0530

    Initial commit

---
+++ client/src/components/home/Feature.jsx
@@ -0,0 +1,53 @@
+import { Zap } from 'lucide-react';
+import React from 'react'
+import Title from './title';
+
+const Feature = () => {
+     const [isHover, setIsHover] = React.useState(false);
+  return (
+    <div id='features' className='flex flex-col items-center my-10 scroll-mt-12'>
+    <div className="flex items-center gap-2 text-sm text-violet-800 bg-violet-600/10  rounded-full px-4 py-1">
+            <Zap width={14}/>
+            <span>easy and efficient</span>
+        </div>        
+
+        <Title title='Smarter Resumes. Less Effort.' description='Let AI design, write, and refine your resume — so you can focus on landing your dream job.' />
+
+            <div className="flex flex-col md:flex-row items-center justify-center xl:-mt-10">
+                <img className="max-w-3xl w-full xl:-ml-32" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/features/group-image-1.png" alt="" />
+                <div className="px-4 md:px-0 space-y-4 md:space-y-6" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+                    <div className={"flex items-center justify-center gap-6 max-w-md group cursor-pointer"}>
+                        <div className={`p-4 flex gap-4 rounded-xl border border-violet-200 shadow-sm bg-violet-80 transition-all duration-300 ease-in-out group-hover:bg-violet-50 group-hover:border-violet-200 ${!isHover ? 'border-violet-100 bg-violet-50' : ''}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="size-6 stroke-violet-600"><path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z" /><circle cx="16.5" cy="7.5" r=".5" fill="currentColor" /></svg>
+                            <div className="space-y-2">
+                                <h3 className="text-base font-semibold text-slate-700">Real-Time Analytics</h3>
+                                <p className="text-sm text-slate-600 max-w-xs">Get instant insights into your finances with live dashboards.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center gap-6 max-w-md group cursor-pointer">
+                        <div className="p-4 flex gap-4 rounded-xl border border-transparent shadow-sm bg-white transition-all duration-300 ease-in-out group-hover:bg-green-50 group-hover:border-green-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="size-6 stroke-green-600"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" /><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" /></svg>
+                            <div className="space-y-2">
+                                <h3 className="text-base font-semibold text-slate-700">Bank-Grade Security</h3>
+                                <p className="text-sm text-slate-600 max-w-xs">End-to-end encryption, 2FA, compliance with GDPR standards.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center gap-6 max-w-md group cursor-pointer">
+                        <div className="p-4 flex gap-4 rounded-xl border border-transparent shadow-sm bg-white transition-all duration-300 ease-in-out group-hover:bg-orange-50 group-hover:border-orange-200">
+                            <svg className="size-6 stroke-orange-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 15V3" /><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><path d="m7 10 5 5 5-5" /></svg>
+                            <div className="space-y-2">
+                                <h3 className="text-base font-semibold text-slate-700">Customizable Reports</h3>
+                                <p className="text-sm text-slate-600 max-w-xs">Export professional, audit-ready financial reports for tax or internal review.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+  )
+}
+
+export default Feature
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/home/Hero'
+import Feature from '../components/home/Feature'
+import Testimonial from '../components/home/Testimonial'
+import Footer from '../components/home/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero/>
+        <Feature/>
+        <Testimonial/>
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
